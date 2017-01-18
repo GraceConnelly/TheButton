70faea3508a882eaf9b7601fa8a54461ba816ae7
Ben Sterrett <raynjamin@gmail.com>
2017-01-18 14:30:43 -0500

    enabling cross-origin things

--- src/main/java/com/example/TheButtonController.java
+++ src/main/java/com/example/TheButtonController.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TheButtonController {
     public static Status currentStatus = new Status();
 
+    @CrossOrigin
     @RequestMapping(path = "/status", method = RequestMethod.GET)
     public Status getStatus() {
         return currentStatus;
     }
 
+    @CrossOrigin
     @RequestMapping(path = "/click", method = RequestMethod.POST)
     public void clickButton() {
         currentStatus.setCurrent(0);
